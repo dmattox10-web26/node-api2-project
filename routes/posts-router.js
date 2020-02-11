f356592d22d03e2f86635e8aac0c9a2f71ed50df
Daniel Mattox <dmattox10@gmail.com>
2020-02-11 17:06:31 -0500

    Created first four routes

---
+++ routes/posts-router.js
@@ -0,0 +1,62 @@
+const express = require('express')
+
+const Posts = require('./data/db.js')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
+})
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    })
+})
+
+router.post('/', (req, res) => {
+    if (req.body.title && req.body.contents) {
+        Posts.insert(req.body)
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+    }
+    else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+})
+
+router.post('/:id/comments', (req, res) => {
+    if (Posts.findById(req.params.id)) {
+        if (req.body.text) {
+            Posts.insertComment(req.body.text)
+            .then(comment => {
+                res.status(201).json(comment)
+            })
+            .catch(err => {
+                res.status(500).json({ error: "There was an error while saving the comment to the database" })
+            })
+        }
+        else { 
+            res.status(400).json({ errorMessage: "Please provide text for the comment." })
+        }
+    }
+    else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
+})
+
+module.exports = router
